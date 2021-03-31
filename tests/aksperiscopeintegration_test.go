5e76259b4c39ff4d0cc15695c54fae69fa21c309
Tatsinnit <tamishra@microsoft.com>
2021-03-31 16:46:24 +1300

    duration flick

--- tests/aksperiscopeintegration_test.go
+++ tests/aksperiscopeintegration_test.go
@@ -16,7 +16,7 @@ func TestEndToEndIntegrationSuccessCase(t *testing.T) {
 	// checkifpodsrunning(t)
 	g.Eventually(func() bool {
 		return checkifpodsrunning(t)
-	}, "60s", "20s").Should(BeTrue())
+	}, "20s", "60s").Should(BeTrue())
 
 }
 
