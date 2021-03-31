1b43821c6feaa7e347607054f383c7a48fa272d6
Tatsinnit <tamishra@microsoft.com>
2021-03-31 13:37:26 +1300

    gomega for test for assertion.

--- tests/aksperiscopeintegration_test.go
+++ tests/aksperiscopeintegration_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/Azure/aks-periscope/pkg/utils"
+	. "github.com/onsi/gomega"
 )
 
 func TestEndToEndIntegrationSuccessCase(t *testing.T) {
@@ -18,14 +19,17 @@ func TestEndToEndIntegrationUnsuccessFulCase(t *testing.T) {
 func runperiscopedeploycommand(t *testing.T, validate bool) {
 	// This flag switch on and off for storage account validation.
 	validateflag := fmt.Sprintf("--validate=%v", validate)
+	g := NewGomegaWithT(t)
 
 	output, err := utils.RunCommandOnContainer("kubectl", "apply", "-f", "../deployment/aks-periscope.yaml", validateflag)
 
 	if err != nil && validate {
+		g.Expect(err).Should(HaveOccurred())
 		t.Logf("unsuccessful output: %v\n", err)
 	}
 
 	if output != "" && !validate {
+		g.Expect(err).ToNot(HaveOccurred())
 		t.Logf("successful output: %v\n", output)
 	}
 }
